312fbece83079a68b5eb704d3ccf967218f4af4f
Aldo Gutierrez <aldodario1@hotmail.es>
2019-11-23 16:02:38 -0800

    Modified the HomeList widget list

--- best_flutter_ui_templates/lib/model/homelist.dart
+++ best_flutter_ui_templates/lib/model/homelist.dart
@@ -1,6 +1,4 @@
-import 'package:best_flutter_ui_templates/designCourse/homeDesignCourse.dart';
 import 'package:best_flutter_ui_templates/fitnessApp/fitnessAppHomeScreen.dart';
-import 'package:best_flutter_ui_templates/hotelBooking/hotelHomeScreen.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeList {
@@ -14,16 +12,8 @@ class HomeList {
 
   static List<HomeList> homeList = [
     HomeList(
-      imagePath: "assets/hotel/hotel_booking.png",
-      navigateScreen: HotelHomeScreen(),
-    ),
-    HomeList(
       imagePath: "assets/fitness_app/fitness_app.png",
       navigateScreen: FitnessAppHomeScreen(),
-    ),
-    HomeList(
-      imagePath: "assets/design_course/design_course.png",
-      navigateScreen: DesignCourseHomeScreen(),
     ),
   ];
 }
