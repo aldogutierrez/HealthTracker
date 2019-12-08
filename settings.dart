bdea2372ea60a53236206bfed35f3a51dadb20bd
slarshsjsu <43052334+slarshsjsu@users.noreply.github.com>
2019-12-07 21:14:44 -0800

    Add files via upload

---
+++ settings.dart
@@ -0,0 +1,13 @@
+import 'package:best_flutter_ui_templates/appTheme.dart';
+import 'package:flutter/material.dart';
+
+class settings extends StatefulWidget {
+  @override
+  _settingsState createState() => _settingsState();
+}
+
+class _settingsState extends State<settings> {
+  @override
+  void initState() {
+    super.initState();
+  }
