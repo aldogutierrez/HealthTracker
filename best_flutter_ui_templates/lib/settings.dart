dfb420c4c341119ae55160bc03e7602305ab88dd
Aldo Gutierrez <aldodario1@hotmail.es>
2019-12-07 21:31:43 -0800

    Placed settings.dart in the correct folder

---
+++ best_flutter_ui_templates/lib/settings.dart
@@ -0,0 +1,96 @@
+import 'package:best_flutter_ui_templates/appTheme.dart';
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppTheme.nearlyWhite,
+      child: SafeArea(
+        top: false,
+        child: Scaffold(
+          backgroundColor: AppTheme.nearlyWhite,
+          body: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).padding.top,
+                    left: 16,
+                    right: 16),
+                child: Image.asset("assets/images/helpImage.png"),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 8),
+                child: Text(
+                  'Settings:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  "Toggle Dark Mode",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Container(
+                      width: 140,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(0.6),
+                              offset: Offset(4, 4),
+                              blurRadius: 8.0),
+                        ],
+                      ),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {},
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Text(
+                                'See manual for questions',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
