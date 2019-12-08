15f7fb5895c8ab04fc6bff130aea8db9de806f9a
Aldo Gutierrez <aldodario1@hotmail.es>
2019-12-07 18:18:20 -0800

    Imported package for dark theme

--- best_flutter_ui_templates/lib/fitnessApp/myDiary/mealsListView.dart
+++ best_flutter_ui_templates/lib/fitnessApp/myDiary/mealsListView.dart
@@ -1,4 +1,5 @@
 import 'package:best_flutter_ui_templates/fitnessApp/fintnessAppTheme.dart';
+import 'package:best_flutter_ui_templates/fitnessApp/fitnessAppThemeDark.dart';
 import 'package:best_flutter_ui_templates/fitnessApp/models/mealsListData.dart';
 import 'package:best_flutter_ui_templates/main.dart';
 import 'package:flutter/material.dart';
