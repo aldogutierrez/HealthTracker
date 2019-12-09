762edfb797a19512f205f606595a0d159ea80164
Aldo Gutierrez <aldodario1@hotmail.es>
2019-12-08 19:14:25 -0800

    Created User table and set foreign key contraints on the other tables

--- best_flutter_ui_templates/lib/data/moor_database.dart
+++ best_flutter_ui_templates/lib/data/moor_database.dart
@@ -2,23 +2,29 @@ import 'package:moor_flutter/moor_flutter.dart';
 
 part 'moor_database.g.dart';
 
-class HeartRate extends Table {
+class User extends Table {
   IntColumn get uID => integer().autoIncrement()();
-  IntColumn get heartRate => integer()();
+  TextColumn get name => text().withLength(min: 1, max: 50)();
+  IntColumn get age => integer()();
+}
 
+class HeartRate extends Table {
+  IntColumn get uID => integer().customConstraint('REFERENCES User(uID)')();
+  IntColumn get heartRate => integer()();
 }
 
 class BloodPressure extends Table {
-  // IntColumn get uID 
+  IntColumn get uID => integer().customConstraint('REFERENCES User(uID)')();
   IntColumn get systolic => integer()();
   IntColumn get diastolic => integer()();
 }
 
 class Weight extends Table {
+  IntColumn get uID => integer().customConstraint('REFERENCES User(uID)')();
   IntColumn get weight => integer()();
 }
 
-@UseMoor(tables: [HeartRate, BloodPressure, Weight])
+@UseMoor(tables: [User, HeartRate, BloodPressure, Weight])
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'db.sqlite', logStatements: true));
 
